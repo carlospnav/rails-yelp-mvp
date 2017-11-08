244fa946d916217f64a40c083f36d077da2ee61b
Carlos Navarrete <carlpnav@gmail.com>
2017-11-08 16:10:27 -0200

    yelp finish

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,10 @@
 class ReviewsController < ApplicationController
   before_action :find_restaurant, only: [ :new, :create ]
 
+  def new
+    @review = Review.new
+  end
+
   def create
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::GameOfThrones.house,
+    address: Faker::GameOfThrones.city,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample)
+  restaurant.save
+
+  review = Review.new(
+    content: Faker::ChuckNorris.fact,
+    rating:(0..5).to_a.sample
+    )
+  review.restaurant = restaurant
+  review.save
+end
+
